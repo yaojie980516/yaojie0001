6661d1c315357b2b70839b8fc3cda01f03a05c59
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-12 08:30:15 +0800

    Update yaojie.js

--- yaojie.js
+++ yaojie.js
@@ -1,18 +1,16 @@
-var mysql= require('mysql'); 
-var pool = mysql.createConnection({ 
-host : '192.168.1.115', 
-port : 3306,
-user : 'root', 
-password : 'eleclong', 
-database : 'mysql' 
-}); 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '192.163.1.115',
+  user     : 'root',
+  password : 'eleclong',
+  database : 'mysql'
+});
 
-exports.getConnection = function(callback) {
-    pool.getConnection(function(err, conn) {
-      if(err) {
-        return callback(err);
-      }
-      callback(err, conn);
-    });
-  };
-pool.end();
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
