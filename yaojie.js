d3ca42b75a5f290515bda36faf6170638aa9c7b2
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-13 19:54:44 +0800

    Update yaojie.js

--- yaojie.js
+++ yaojie.js
@@ -1,6 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '192.168.1.115',
+  port     : '3306',
   user     : 'root',
   password : 'eleclong',
   database : 'mysql'
