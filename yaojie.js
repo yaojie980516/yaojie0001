b5f10b6664747d7321dc7414b577ba229644fa60
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-13 20:00:44 +0800

    Update yaojie.js

--- yaojie.js
+++ yaojie.js
@@ -7,8 +7,8 @@ var connection = mysql.createConnection({
   database : 'mysql'
 });
 
-exports.getConnection = function(callback){
- connection.getConnection(functuin(err, conn){
+exports.getConnection = function(callback) {
+ connection.getConnection(functuin(err, conn) {
       if(err) {
           return callback(err);
          }
