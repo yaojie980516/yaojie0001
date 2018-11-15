52ffc88989695b0f04020d9ac9e8facc51fab90c
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-15 21:26:55 +0800

    Update yaojie.js

--- yaojie.js
+++ yaojie.js
@@ -1,8 +1,3 @@
-var http = require('http');
-http.createServer(function (request, response){
-  response.writeHead(200, {'Content_Type': 'text/plain'});
-  response.end('Hello World\n');
-  }).listen(80);
 var mysql      = require('mysql');
 var connection = mysql.createPool({
   connectionLimit: 100,
