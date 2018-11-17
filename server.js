6a104b0bb6da46cb22f95e4304a2b6cacf6fe4ba
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-17 20:21:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http'); 
 http.createServer(function (request, response){ 
-  response.setHeader('Content_Type', 'text/html;charset=utf-8') 
-  response.write('<h1>你好NodeJS</h1>')  
-  response.end() 
+  response.setHeader('Content_Type', 'text/html;charset=utf-8')， 
+  response.write('<h1>你好NodeJS</h1>')，
+  response.end()，
   }).listen(68) 
 console.log('server running at http://122.152.214.116:68/') 
