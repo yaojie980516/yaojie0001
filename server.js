898d8b9753a65d854d1572861ebe0215703c7da5
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-17 21:45:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http'); 
 http.createServer(function (request, response){ 
   response.setHeader('Content_Type', 'text/html;charset=utf-8') 
-  response.write('<h1>你好NodeJS</h1>')
+  response.write('<h1>Holle NodeJS you succeed !</h1>')
   response.end()
   }).listen(88) 
 console.log('server running at http://122.152.214.116:88/') 
