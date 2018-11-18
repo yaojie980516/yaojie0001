cd62ff8ef2365eb43c1f2c8b12bf08c2e734fa0d
yaojie980516 <39930552+yaojie980516@users.noreply.github.com>
2018-11-18 20:29:08 +0800

    Update yaojie.js

--- yaojie.js
+++ yaojie.js
@@ -9,7 +9,28 @@ var connection = mysql.createConnection({
   debug: false,
   multipleStatements: true
 });
-
+connection.connect(function(err){
+    if(err){        
+          console.log('[query] - :'+err);
+        return;
+    }
+      console.log('[connection connect]  succeed!');
+});  
+//执行SQL语句
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) { 
+     if (err) {
+             console.log('[query] - :'+err);
+        return;
+     }
+     console.log('The solution is: ', rows[0].solution);  
+});  
+//关闭connection
+connection.end(function(err){
+    if(err){        
+        return;
+    }
+      console.log('[connection end] succeed!');
+});
 console.log('Hello World!');
 
 
